e7a735ffe9d46e28dda432c3bab22b4d28fdd5eb
RamaPratheeba <rama.pratheeba@idynamics.com>
2024-03-08 11:47:58 +0530

    Added button.

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.lblDemo = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblDemo
@@ -53,11 +54,22 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "First change";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Click Me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblDemo);
             this.Name = "Form1";
@@ -71,5 +83,6 @@
 
         private Label lblDemo;
         private Label label1;
+        private Button button1;
     }
 }
