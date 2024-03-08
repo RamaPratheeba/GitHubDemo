09c1525085a5ce7a4dbf53a95f1965f6cb8774ef
RamaPratheeba <RamaPratheeba@IDV0002>
2024-03-08 11:42:28 +0530

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDemo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDemo
+            // 
+            this.lblDemo.AutoSize = true;
+            this.lblDemo.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDemo.Location = new System.Drawing.Point(263, 138);
+            this.lblDemo.Name = "lblDemo";
+            this.lblDemo.Size = new System.Drawing.Size(282, 54);
+            this.lblDemo.TabIndex = 0;
+            this.lblDemo.Text = "GitHub Demo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDemo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblDemo;
+    }
+}
